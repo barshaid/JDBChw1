baf1a20c47c8893f92fc0c1f2450a6cb2a10ab2e
barshaid <exofang@hotmail.com>
2020-01-07 18:34:22 +0200

    2nd commit

--- src/JDBChw1.java
+++ src/JDBChw1.java
@@ -1,10 +1,15 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class JDBChw1 {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 
+public class JDBChw1 {
+	static Scanner sc = new Scanner(System.in);
+	static Scanner sc2 = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+
+		char choice = '0';
 		// removal of Timezone and Datetimecode legcay mode parameters from connection
 		// URL might be
 		// required, depends on MySQL version
@@ -14,19 +19,25 @@ public class JDBChw1 {
 		String user = sc.next();
 		System.out.println("Password:");
 		String pw = sc.next();
-		Connection con = DriverManager.getConnection(db, user, pw);
-		Statement stmt = con.createStatement();
+		Connection con = null;
+		Statement stmt = null;
+		try {
+			con = DriverManager.getConnection(db, user, pw);
+			stmt = con.createStatement();
+		} catch (CommunicationsException e) {
+			System.out.println("Connection error \nterminating");
+			choice = 'd';
+		}
 
-		char choice = '0';
 		while (choice != 'd') {
 			System.out.println("Please select from the menu:");
-			System.out.println("a.Add actor");
-			System.out.println("b.Execute query");
-			System.out.println("c.Execute parameter query");
-			System.out.println("d.Exit");
+			System.out.println("a. Add actor");
+			System.out.println("b. Execute query");
+			System.out.println("c. Execute parameter query");
+			System.out.println("d. Exit");
 
 			choice = sc.next().charAt(0);
-			
+
 			switch (choice) {
 			case 'a':
 				addActor(stmt);
@@ -44,6 +55,8 @@ public class JDBChw1 {
 				System.out.println("Exiting");
 				stmt.close();
 				con.close();
+				sc.close();
+				sc2.close();
 				break;
 
 			default:
@@ -51,6 +64,7 @@ public class JDBChw1 {
 			}
 			System.out.println();
 		}
+		sc.close();
 
 	}
 
@@ -69,35 +83,41 @@ public class JDBChw1 {
 			System.out.println("\n");
 			j++;
 		}
-		System.out.println(j + " enteries returned");
+		System.out.println(j + " enterie(s) returned");
 		rs.close();
 	}
 
 	public static void addActor(Statement stmt) throws SQLException {
 		String fname, lname;
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first name:");
 		fname = sc.nextLine();
 		System.out.println("Enter last name:");
 		lname = sc.nextLine();
-		stmt.executeUpdate("INSERT INTO ACTOR(FIRST_NAME, LAST_NAME) VALUES(" + "\"" + fname + "\"" + "," + "\"" + lname
-				+ "\"" + ");");
+		try {
+			stmt.executeUpdate("INSERT INTO ACTOR(FIRST_NAME, LAST_NAME) VALUES(" + "\"" + fname + "\"" + "," + "\""
+					+ lname + "\"" + ");");
+			System.out.println("Entry successfuly added");
+		} catch (SQLException e) {
+			System.out.println("Error\nentry was not a");
+			return;
+		}
 	}
 
 	public static void quary(Statement stmt) throws SQLException {
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 		System.out.println("Enter query:");
 		String query = sc.nextLine();
 		ResultSet rs = null;
 		try {
-		rs = stmt.executeQuery(query);
-		}catch(SQLException e) {
+			rs = stmt.executeQuery(query);
+		} catch (SQLException e) {
 			System.out.println("Illegal query\nReturning to menu");
 			return;
 		}
 		ResultSetMetaData metaData = rs.getMetaData();
 		int columnCount = metaData.getColumnCount();
-		int i = 1, j = 0; 
+		int i = 1, j = 0;
 
 		while (rs.next()) {
 			i = 1;
@@ -108,24 +128,22 @@ public class JDBChw1 {
 			System.out.println();
 			j++;
 		}
-		System.out.println("Query returned " + j + "entries");
+		System.out.println("Query returned " + j + " entrie(s)");
 		rs.close();
 
 	}
 
 	public static void pQuary(Connection con) throws SQLException {
-		Scanner sc = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
 		char choice;
 		ResultSet rs;
 		String temp;
 		System.out.println("Please select from the menu:");
-		System.out.println("a.Find movie by keyword in name");
-		System.out.println("b.Find movie by actor name");
-		System.out.println("c.Find movie by year");
-		System.out.println("d.Find movie by category");
-		System.out.println("e.Find movie by acting cast size");
-		System.out.println("enter anything else to return");
+		System.out.println("a. Find movie by keyword in name");
+		System.out.println("b. Find movie by actor name");
+		System.out.println("c. Find movie by year");
+		System.out.println("d. Find movie by category");
+		System.out.println("e. Find movie by acting cast size");
+		System.out.println("Enter anything else to return");
 
 		choice = sc.next().charAt(0);
 		PreparedStatement ps;
@@ -156,8 +174,7 @@ public class JDBChw1 {
 		case 'c':
 			System.out.println("Enter release year:");
 			temp = sc.next();
-			ps = con.prepareStatement(
-					"SELECT TITLE FROM FILM WHERE RELEASE_YEAR=?");
+			ps = con.prepareStatement("SELECT TITLE FROM FILM WHERE RELEASE_YEAR=?");
 			ps.setObject(1, temp);
 			rs = ps.executeQuery();
 			print(ps, rs);
@@ -177,7 +194,7 @@ public class JDBChw1 {
 			System.out.println("Enter number of actors");
 			int count = sc.nextInt();
 			ps = con.prepareStatement(
-					"SELECT TITLE FROM FILM WHERE FILM_ID IN (SELECT ACTOR_ID FROM FILM_ACTOR GROUP BY FILM_ID HAVING COUNT(ACTOR_ID)=?) ");
+					"SELECT TITLE FROM FILM WHERE FILM_ID IN (SELECT FILM_ID FROM FILM_ACTOR GROUP BY FILM_ID HAVING COUNT(ACTOR_ID)=?) ");
 			ps.setObject(1, count);
 			rs = ps.executeQuery();
 			print(ps, rs);
